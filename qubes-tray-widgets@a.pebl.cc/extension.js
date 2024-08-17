62b94c51ae92937dcada1c562a716d1963597f14
apebl <a@pebl.cc>
2024-08-17 14:19:02 +0000

    feat: Add network widget

--- qubes-tray-widgets@a.pebl.cc/extension.js
+++ qubes-tray-widgets@a.pebl.cc/extension.js
@@ -12,8 +12,9 @@ const _ = ExtensionUtils.gettext
 const Me = ExtensionUtils.getCurrentExtension()
 
 Gio._promisify(Gio.Subprocess.prototype, 'communicate_utf8_async')
+Gio._promisify(Gio.Subprocess.prototype, 'wait_async')
 
-async function cmd(argv, cancellable = null) {
+const cmd = async (argv, cancellable = null) => {
   const flags = Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
   const proc = new Gio.Subprocess({ argv: argv, flags: flags })
   proc.init(cancellable)
@@ -39,10 +40,27 @@ async function cmd(argv, cancellable = null) {
   }
 }
 
+const spawn = async (argv, cancellable = null) => {
+  const flags = Gio.SubprocessFlags.NONE
+  const proc = new Gio.Subprocess({ argv: argv, flags: flags })
+  proc.init(cancellable)
+
+  const cancel_id = cancellable ? cancellable.connect(() => proc.force_exit()) : 0
+
+  try {
+    await proc.wait_async(cancellable)
+    return proc.get_successful()
+  } finally {
+    if (cancel_id > 0) {
+      cancellable.disconnect(cancel_id)
+    }
+  }
+}
+
 const TrayWidget = GObject.registerClass(
   {},
   class TrayWidget extends St.Button {
-    #indicator
+    _indicator
     #command
 
     constructor(indicator, command, icon_name) {
@@ -50,7 +68,7 @@ const TrayWidget = GObject.registerClass(
         child: new St.Icon({ icon_name: icon_name }),
         style_class: 'panel-button system-status-icon',
       })
-      this.#indicator = indicator
+      this._indicator = indicator
       this.#command = command
     }
 
@@ -71,11 +89,46 @@ const TrayWidget = GObject.registerClass(
 
     async show_menu() {
       const promise = cmd(['pgrep', this.#command])
-      this.#indicator.menu.close()
+      this._indicator.menu.close()
       const output = await promise
       const pid = output.split('\n')[0]
       if (!pid) throw new Error(`Could not find process: ${this.#command}`)
       await cmd(['kill', '-USR1', pid])
+    }
+  },
+)
+
+const NetworkTrayWidget = GObject.registerClass(
+  {},
+  class NetworkTrayWidget extends TrayWidget {
+    #settings
+
+    constructor(indicator) {
+      super(indicator, null, 'network-wireless-symbolic')
+      this.#settings = ExtensionUtils.getSettings()
+    }
+
+    async show_menu() {
+      const netvm = this.#settings.get_value('netvm').unpack()
+      const argv = this.#settings.get_value('netvm-command').deep_unpack()
+      this._indicator.menu.close()
+      if (argv.length === 0) {
+        const gnome_settings = this.#check_command(netvm, 'gnome-control-center')
+        const nm_editor = this.#check_command(netvm, 'nm-connection-editor')
+
+        if (await gnome_settings) {
+          await spawn(['qvm-run', netvm, 'gnome-control-center', 'wifi'])
+        } else if (await nm_editor) {
+          await spawn(['qvm-run', netvm, 'nm-connection-editor'])
+        }
+        throw new Error('No available network menu commands')
+      } else {
+        await spawn(['qvm-run', netvm, ...argv])
+      }
+    }
+
+    async #check_command(netvm, command) {
+      return await spawn(['qvm-run', netvm, 'sh', '-c', `command -v ${command}`])
     }
   },
 )
@@ -105,6 +158,7 @@ const Indicator = GObject.registerClass(
     }
 
     #setup_widgets() {
+      this.#network_widget()
       this.#widget('qui-clipboard', 'edit-copy')
       this.#widget('qui-devices', 'qubes-devices')
       this.#widget('qui-disk-space', 'drive-harddisk')
@@ -114,6 +168,11 @@ const Indicator = GObject.registerClass(
 
     #widget(command, icon_name) {
       const widget = new TrayWidget(this, command, icon_name)
+      this.#widgets.add_child(widget)
+    }
+
+    #network_widget() {
+      const widget = new NetworkTrayWidget(this)
       this.#widgets.add_child(widget)
     }
   },
